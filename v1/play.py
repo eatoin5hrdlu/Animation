5f3c46b8d0883ed6e31222c3ef2baead87029f8b
jdiver919 <jdiver919@yahoo.com>
2014-06-25 15:42:48 -0400

    updated code to play the movie

--- v1/play.py
+++ v1/play.py
@@ -1,3 +1,8 @@
+import subprocess
+
+##mplayer -mf fps=25:type=jpg *.jpg mf://*.jpg
 def playVideo ():
 	print "playVideo"
-##mplayer -mf fps=25:type=jpg *.jpg mf://*.jpg
+	# Temporarily use mplayer to render and play the video
+	videocmd = "mplayer -mf fps=25:type=jpg *.jpg mf://./frames/*.jpg"
+	process = subprocess.call(videocmd, shell=True)
